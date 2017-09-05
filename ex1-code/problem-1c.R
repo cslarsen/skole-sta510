b022d0f6b5dc23949844f9cbe06eb969884565d7
Christian Stigen Larsen <csl@csl.name>
2017-09-05 23:56:11 +0200

    Problem 1c

---
+++ ex1-code/problem-1c.R
@@ -0,0 +1,24 @@
+# Lecture code example modified by Christian Stigen
+
+# Reset environment for interactive runs
+rm(list=ls())
+
+# As in problem 1 (b), but with 0.6 chance of heads.
+simheads <- function(Nsim,k) {
+  nheads <- vector(length=Nsim)
+  for(i in 1:Nsim)
+    nheads[i] <- sum(sample(0:1, size=k, replace=TRUE, prob=c(0.4, 0.6)))
+  return(nheads)
+}
+
+par(mfrow=c(3,1))
+
+for (Nsim in c(10, 20, 1000)) {
+  k <- 5
+  nheadsim <- simheads(Nsim=Nsim,k=k)
+  relfreq <- table(nheadsim) / Nsim
+  barplot(relfreq,
+          ylab="Relative frequency",
+          xlab=paste("Heads in ", k, " coin flips (", Nsim, " simulations)",
+                     sep=""))
+}

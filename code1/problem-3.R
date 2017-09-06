96c8474b4fdc4bd043f712fd8a4a35b2557c1c72
Christian Stigen Larsen <csl@csl.name>
2017-09-07 00:12:49 +0200

    Fixed the damn code

--- code1/problem-3.R
+++ code1/problem-3.R
@@ -1,20 +1,19 @@
-iterations <- 1e5
+iterations <- 1e4
 
-# Approximates E(X) for a PMF by trial
-sim_ev <- function(domain, weights, samples=iterations) {
+# Simulated expected value for a PMF
+sev <- function(domain, weights, samples=iterations) {
   total <- 0
 
-  # Draws a weighted sample and updates the running total
   for (i in 1:samples) {
-    drawn <- sample(domain, size=1, prob=weights)
-    total <- total + drawn
+    draw <- sample(domain, size=1, prob=weights)
+    total <- total + draw
   }
 
   average <- total / samples
 }
 
-# Calculates exact E(X) for a PMF
-exact_ev <- function(domain, weights) {
+# Exact expected value for a PMF
+eev <- function(domain, weights) {
   sum(domain * weights)
 }
 
@@ -22,17 +21,17 @@ cat("Results for", iterations, "iterations per simulation\n\n")
 
 f <- function(x) { x/10 }
 g <- function(x) { x^3 }
-domain <- 1:4
+D <- 1:4 # domain
 
-cat("(2a) E(X) simulated    ", sim_ev(domain, f(domain)), "\n")
-cat("(2a) E(X) exact        ", exact_ev(domain, f(domain)), "\n\n")
-cat("(2a) E(g(X)) simulated ", sim_ev(g(domain), f(domain)), "\n")
-cat("(2a) E(g(X)) exact     ", exact_ev(g(domain), f(domain)), "\n\n")
+cat("(2a) E(X) simulated    ", sev(D, f(D)), "\n")
+cat("(2a) E(X) exact        ", eev(D, f(D)), "\n\n")
+cat("(2a) E(g(X)) simulated ", sev(g(D), f(D)), "\n")
+cat("(2a) E(g(X)) exact     ", eev(g(D), f(D)), "\n\n")
 
 f <- function(x, c=1/30) { c*(x+1)^2 }
-domain <- 0:3
+D <- 0:3 # domain
 
-cat("(2b) E(X) simulated    ", sim_ev(domain, f(domain)), "\n")
-cat("(2b) E(X) exact        ", exact_ev(domain, f(domain)), "\n\n")
-cat("(2b) E(g(X)) simulated ", sim_ev(g(domain), f(domain)), "\n")
-cat("(2b) E(g(X)) exact     ", exact_ev(g(domain), f(domain)), "\n")
+cat("(2b) E(X) simulated    ", sev(D, f(D)), "\n")
+cat("(2b) E(X) exact        ", eev(D, f(D)), "\n\n")
+cat("(2b) E(g(X)) simulated ", sev(g(D), f(D)), "\n")
+cat("(2b) E(g(X)) exact     ", eev(g(D), f(D)), "\n")

387e49682549c3c7dce0fc7658fc3f2c6c3ef02c
Christian Stigen Larsen <csl@csl.name>
2017-09-07 00:07:41 +0200

    Fixed the damn code

--- code1/problem-3.R
+++ code1/problem-3.R
@@ -1,9 +1,7 @@
 iterations <- 1e5
 
-# Find the expected value of a PMF by returning the average of repeated,
-# weighted samples.
-sim_ev <- function(domain, pmf, samples=iterations) {
-  weights = pmf(domain)
+# Approximates E(X) for a PMF by trial
+sim_ev <- function(domain, weights, samples=iterations) {
   total <- 0
 
   # Draws a weighted sample and updates the running total
@@ -13,33 +11,28 @@ sim_ev <- function(domain, pmf, samples=iterations) {
   }
 
   average <- total / samples
-  normalized <- average * sum(weights)
 }
 
-# Calculates E(X) for a PMF exactly by x*f(x) for all x in domain
-exact_ev <- function(domain, pmf) {
-  sum(domain * pmf(domain))
+# Calculates exact E(X) for a PMF
+exact_ev <- function(domain, weights) {
+  sum(domain * weights)
 }
-
-# Provide a domain and PMF to find E(X):
-
-f <- function(x, c=10) { x/c }
-g <- function(x) { x^3 }
-domain <- 1:4
 
 cat("Results for", iterations, "iterations per simulation\n\n")
 
-cat("(2a) E(X) simulated    ", sim_ev(domain, f), "\n")
-cat("(2a) E(X) exact        ", exact_ev(domain, f), "\n\n")
+f <- function(x) { x/10 }
+g <- function(x) { x^3 }
+domain <- 1:4
 
-cat("(2a) E(g(X)) simulated ", sim_ev(g(domain), f), "\n")
-cat("(2a) E(g(X)) exact     ", exact_ev(g(domain), f), "\n\n")
+cat("(2a) E(X) simulated    ", sim_ev(domain, f(domain)), "\n")
+cat("(2a) E(X) exact        ", exact_ev(domain, f(domain)), "\n\n")
+cat("(2a) E(g(X)) simulated ", sim_ev(g(domain), f(domain)), "\n")
+cat("(2a) E(g(X)) exact     ", exact_ev(g(domain), f(domain)), "\n\n")
 
 f <- function(x, c=1/30) { c*(x+1)^2 }
 domain <- 0:3
 
-cat("(2b) E(X) simulated    ", sim_ev(domain, f), "\n")
-cat("(2b) E(X) exact        ", exact_ev(domain, f), "\n\n")
-
-cat("(2b) E(g(X)) simulated ", sim_ev(g(domain), f), "\n")
-cat("(2b) E(g(X)) exact     ", exact_ev(g(domain), f), "\n")
+cat("(2b) E(X) simulated    ", sim_ev(domain, f(domain)), "\n")
+cat("(2b) E(X) exact        ", exact_ev(domain, f(domain)), "\n\n")
+cat("(2b) E(g(X)) simulated ", sim_ev(g(domain), f(domain)), "\n")
+cat("(2b) E(g(X)) exact     ", exact_ev(g(domain), f(domain)), "\n")

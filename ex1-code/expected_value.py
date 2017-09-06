8a88e1466d3004304542893ad5cedfcc94db9071
Christian Stigen Larsen <csl@csl.name>
2017-09-06 20:22:41 +0200

    Adds expected value simulator

---
+++ ex1-code/expected_value.py
@@ -0,0 +1,37 @@
+import random
+
+def f(x, c=1.0/30.0):
+    return c*(x+1)*(x+1)
+
+# choices = [(value, weight), ...]
+def weighted_choice(choices):
+   total = sum(w for c, w in choices)
+   r = random.uniform(0, total)
+   upto = 0
+   for c, w in choices:
+      if upto + w >= r:
+         return c
+      upto += w
+   assert False, "Shouldn't get here"
+
+def simulate_expected_value(function, x_values, iterations=100000):
+    choices = list(map(lambda x: (x, f(x)), x_values))
+
+    total = 0
+    for n in range(iterations):
+        sample = weighted_choice(choices)
+        total += sample
+
+    return float(total) / float(iterations)
+
+def f(x, c=10.0):
+    return float(x) / c
+
+def g(x, c=1.0/30.0):
+    return c*(x+1)**2
+
+e1 = simulate_expected_value(f, (1,2,3,4))
+print("E(X) for problem 2a: %s" % e1)
+
+e2 = simulate_expected_value(g, (0,1,2,3))
+print("E(X) for problem 2b: %s" % e2)

a6c59bdc09fc807fbd7724eeff70a424d7903ee3
Christian Stigen Larsen <csl@csl.name>
2017-09-06 21:47:29 +0200

    Adds E(g(X))

--- code1/problem-3.R
+++ code1/problem-3.R
@@ -1,5 +1,7 @@
-sim_expected <- function(domain, f, samples=1e4) {
-  weights = f(domain)
+# Find the expected value of a PMF by returning the average of repeated,
+# weighted samples.
+sim_expected <- function(domain, pmf, samples=1e4) {
+  weights = pmf(domain)
   total <- 0
 
   # Draws a weighted sample and updates the running total
@@ -13,8 +15,16 @@ sim_expected <- function(domain, f, samples=1e4) {
 
 # Provide a domain and PMF to find E(X):
 
-E <- sim_expected(1:4, function(x, c=10) { x/c })
-cat("(2a) Expected value:", E, "\n")
+f <- function(x, c=10) { x/c }
+EX <- sim_expected(1:4, f)
+Eg <- sim_expected(1:4, function(x) { f(x)^3 })
 
-E <- sim_expected(0:3, function(x, c=1/30) { c*(x+1)^2 })
-cat("(2a) Expected value:", E, "\n")
+cat("(2a) E(X) =", EX, "\n")
+cat("(2a) E(g(X)) =", Eg, "\n\n")
+
+f <- function(x, c=1/30) { c*(x+1)^2 }
+EX <- sim_expected(0:3, f)
+Eg <- sim_expected(0:3, function(x) { f(x)^3 })
+
+cat("(2b) E(X) =", EX, "\n")
+cat("(2b) E(g(X)) =", Eg, "\n")

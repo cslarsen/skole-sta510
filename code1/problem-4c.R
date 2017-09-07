b58658e4acb931dd486b0659ba8823d0d1d0ef83
Christian Stigen Larsen <csl@csl.name>
2017-09-07 22:38:42 +0200

    Problem 4

--- code1/problem-4c.R
+++ code1/problem-4c.R
@@ -8,5 +8,19 @@ f <- function(x) {
   }
 }
 
+F <- function(x) {
+  if ( x < 0 ) {
+    0
+  } else if ( x > 1 ) {
+    1
+  } else {
+    2*x^2 - x^4
+  }
+}
+
+par(mfrow=c(2,1))
 x <- seq(-0.5, 1.5, 0.01)
 plot(x, mapply(f, x), type="l", ylab="f")
+title("Probability density function")
+plot(x, mapply(F, x), type="l", ylab="F")
+title("Cumulative density function")

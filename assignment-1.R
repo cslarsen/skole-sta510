65bfcdf8ec1c13628eb65c02f80acd8db92d42c7
Christian Stigen Larsen <csl@csl.name>
2017-09-18 22:09:20 +0200

    problem 1b

--- assignment-1.R
+++ assignment-1.R
@@ -1,1 +1,14 @@
+# Mandatory Excercise 1
+# Written by Christian Stigen
+
+# Clear any existing environment
 rm(list=ls())
+
+# A variadic helper-function to print stuff to the console. Meaning you can run
+# with Rscript.
+println <- function(...) {
+  cat(paste(..., "\n", sep=""))
+}
+
+# Problem 1 (b)
+println("Problem 1 (b) P(X > 4000) = ", 1 - pexp(4000, rate=1/5000))

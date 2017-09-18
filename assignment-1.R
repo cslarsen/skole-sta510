d20bbc173a1991c1cc364ac1c8ff911f43e65a34
Christian Stigen Larsen <csl@csl.name>
2017-09-18 23:08:03 +0200

    problem 1c

--- assignment-1.R
+++ assignment-1.R
@@ -16,3 +16,18 @@ println("Problem 1 (b)")
 println("P(X > 4000) = ", 1 - pexp(4000, rate=1/5000))
 println("P(4000 <= X <= 6000) = ",
         pexp(6000, rate=1/5000) - pexp(4000, rate=1/5000))
+println("\n")
+
+# Simulate that the lifetime is more than 4000 hours
+println("Problem 1 (c)")
+
+# Returns the probability (via simulation) that a lightbulb lives longer than
+# 4000 hours.
+P_longer <- function(x, sims, beta=5000) {
+  sum(rexp(sims, 1/beta) > x) / sims
+}
+
+println("P(X > 4000) 10 simulations: ", P_longer(4000, 10))
+println("P(X > 4000) 100 simulations: ", P_longer(4000, 100))
+println("P(X > 4000) 10000 simulations: ", P_longer(4000, 10000))
+println("P(X > 4000) 100000 simulations: ", P_longer(4000, 100000))

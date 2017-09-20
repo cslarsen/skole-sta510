020fb6d103ed312eb141f8f68c9f3efec9e56bc5
Christian Stigen Larsen <csl@csl.name>
2017-09-20 23:58:40 +0200

    comment

--- assignment-1.R
+++ assignment-1.R
@@ -3,12 +3,13 @@
 # UiS, September 2017
 # Written by Christian Stigen
 # ==============================
+#
+# Each problem has been made into a separate function. For example, you can
+# call problem1b() to see the code for that.
 
 # Clear any existing environment
 rm(list=ls())
 
-# A variadic helper-function to print stuff to the console. Meaning you can run
-# with Rscript.
 println <- function(...) {
   cat(..., "\n", sep="")
 }

50ec70ea4f3024881aa3b72caf443b3d85011d8f
Christian Stigen Larsen <csl@csl.name>
2017-10-13 23:37:09 +0200

    prob1b-more

--- assignment-2.R
+++ assignment-2.R
@@ -25,20 +25,26 @@ if ( !"mvtnorm" %in% rownames(installed.packages()) ) {
   library(mvtnorm)
 }
 
-problem1b <- function() {
-  # covariance matrix
+problem1b <- function(runs=1000000) {
+  # Covariance matrix, as calculated in problem 1 (a).
   sigma <- matrix(c( 900, -240,  24,
                     -240,  100, -12,
                       24,  -12,  16), ncol=3)
+  # Mu-vector
   mean <- c(90, 48, 18)
 
-  x <- rmvnorm(n=100000, sigma=sigma, mean=mean)
-  cat(colMeans(x), "\n")
-  cat(var(x), "\n")
-  plot(x)
+  # Perform simulation
+  x <- rmvnorm(n=runs, sigma=sigma, mean=mean)
+  x1 <- x[,1]
+  x2 <- x[,2]
+  x3 <- x[,3]
+
+  # P (X1 > 100, X2 > 50, X3 > 20)
+  println("P(X1 > 100, X2 > 50, X3 > 20) = ",
+          (sum(x1 > 100 & x2 > 50 & x3 > 20) / runs),
+          " (", runs, " simulations)")
 
   # X1 ~ N(my=90, var=900)
   # X2 ~  48, 100
   # X3 ~ 18, 16
-  # P (X1 > 100, X2 > 50, X3 > 20)
 }
